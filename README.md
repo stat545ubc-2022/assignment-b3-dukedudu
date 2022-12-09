60a0b02ad3fcf76659d91b6b65047d2b3a33be0e
Duke Du <dukedu98@gmail.com>
2022-12-09 14:52:52 -0800

    assignment 4

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
-assignment_b3
+# STAT 545 Assignment_b3 & b4
 ================
 Mingyuan Du
-2022-12-01
+2022-12-09
+This is the repository for STAT 545B - Assignment 3 & 4. It contains two shiny apps: an improved BC Liquor App (Assignment 3 Option A) and an improved BC Liquor App with different functions (Assignment 4 Option C).
 Assignment 3 Option A
+## Assignment 4
+The previous shiny app has been reconstructed. Now the user can select from liquor under different sweetness and to explore the relationship between alcohol content and prices. Visit the app at https://dukedududu.shinyapps.io/assignment-b3-dukedudu/.
+### Overview of app features
+* **UI Features**
+  * (new) added a pre-defined theme for the shiny app.
+  * (old) added a liquor images and using HTML format to change the image size.
+  * (new) used heading format and center the header.
+  * (new) added a download button that user can download the current query result.
+  * (new) added a tab view to separate plot and query table results.
+  * (old) added a background color.
+
+* **Functionality Features**
+  * (new) added a hyperlink for the raw dataset.
+  * (new) user can download the current query results.
+  * (new) added a scatter plot for Alcohol_Content and Prices.
+
+## Assignment 3
 Visit the app at https://dukedududu.shinyapps.io/assignment-b3-dukedudu/
-## Overview BCL Query and Plot App
+### Overview BCL Query and Plot App
 The goal of this assignment is to build a shiny app to query the bcl data.
 Users can use this app to find prices of liquor from different countries and the count plot cross the alcohol content.
 
-## Added Features
+### Added Features
 1. selectInput block
 2. insert image
 3. background color
--- app.R
+++ app.R
@@ -1,38 +1,50 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
-library(datateachr) # for cancer_sample dataset
 library(tidyverse)
 library(shinyWidgets)
+library(shinythemes)
 bcl <- read.csv("https://raw.githubusercontent.com/daattali/shiny-server/master/bcl/data/bcl-data.csv")
 options(shiny.autoreload = TRUE)
 ui <- fluidPage(
-  img(src='liquor.jpg', align = "right", height="30%", width="30%"),
-  titlePanel("BC Liquor Store prices"),
+  # Feature 1 (new launched): added a pre-defined theme for the shiny app.
+  theme = shinytheme("sandstone"),
+  # Feature 2 (modified): added a liquor images and using HTML format to change the image size.
+  HTML('<center><img src="liquor.jpg" width="1000"></center>'),
+  # Feature 3 (new launched): used heading format and center the header.
+  titlePanel(h1("BC Liquor Store prices", align = "center")),
   sidebarLayout(
+    # Designed a new shiny app with liquor data. Users can select alcohol from different countries and sweetness.
     sidebarPanel(
-      sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      sliderInput("sweetnessInput", "Sweetness", 0, 10, c(1, 2), pre = "Sweet"),
       radioButtons("typeInput", "Product type",
                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                    selected = "WINE"),
-      # Feature 1: adding selectInput feature to the shiny app to query multiple countries' results
+      # Feature 4 (previous implemented): added selectInput feature to the shiny app to query multiple countries' results
       selectInput("countryInput", "Select a country", choices = bcl$Country, selected = "CANADA", multiple = TRUE),
+      # Feature 5 (new launched): added a download button that user can download the current query result.
+      downloadButton("download")
     ),
-
     mainPanel(
-      ##Feature 2: added an image of Liquor.
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
+      # Feature 6 (new launched): added a tab view to separate plot and query table results.
+      tabsetPanel(
+        tabPanel("Scatter Plot", plotOutput("scatterplot")),
+        tabPanel("Histo Plot", plotOutput("histoplot")),
+        tabPanel("Table", tableOutput("results"))
+      ),
     )
   ),
-  # Feature 3: add a background color
+
+  # Feature 7 (previous implemented): added a background color
   setBackgroundColor(
     color = "ghostwhite",
     gradient = c("linear", "radial"),
     direction = c("bottom", "top", "right", "left"),
     shinydashboard = FALSE
-  )
+  ),
+  # Feature 8 (new launched): added a hyperlink for the raw dataset.
+  a(href="https://raw.githubusercontent.com/daattali/shiny-server/master/bcl/data/bcl-data.csv",
+    "Link to the original data set")
 
 )
 
@@ -49,14 +61,33 @@ server <- function(input, output) {
     }
 
     bcl %>%
-      filter(Price >= input$priceInput[1],
-             Price <= input$priceInput[2],
+      filter(Sweetness >= input$sweetnessInput[1],
+             Sweetness <= input$sweetnessInput[2],
              Type == input$typeInput,
              Country == input$countryInput
       )
   })
 
-  output$coolplot <- renderPlot({
+  # Feature 5 (new launched): user can download the current query results.
+  output$download <-
+    downloadHandler(
+      filename = function(){
+        "tmp_download.csv"
+      },
+      content = function(file){
+        write.csv(filtered(), file)
+      }
+    )
+
+  output$scatterplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
+    ggplot(filtered(), aes(Alcohol_Content, Price, colour = Country)) +
+      geom_point(alpha = 1, size=3)
+  })
+
+  output$histoplot <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
@@ -67,6 +98,7 @@ server <- function(input, output) {
   output$results <- renderTable({
     filtered()
   })
+
 }
 
 shinyApp(ui = ui, server = server)
